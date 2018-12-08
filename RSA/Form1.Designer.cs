3f0c593f4e48728821d189dc39fe0953a8320a85
dat <dothanhdatdo@gmail.com>
2018-12-08 18:36:59 +0700

    1

---
+++ RSA/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace RSA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.soP = new System.Windows.Forms.TextBox();
+            this.soQ = new System.Windows.Forms.TextBox();
+            this.soPhiN = new System.Windows.Forms.TextBox();
+            this.soN = new System.Windows.Forms.TextBox();
+            this.soE = new System.Windows.Forms.TextBox();
+            this.soD = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.rsa_BanRo = new System.Windows.Forms.RichTextBox();
+            this.btMaHoa = new System.Windows.Forms.Button();
+            this.rsa_BanMaHoa = new System.Windows.Forms.RichTextBox();
+            this.rsa_banGiaiMa = new System.Windows.Forms.RichTextBox();
+            this.btGiaiMa = new System.Windows.Forms.Button();
+            this.rsa_banMaHoaGuiDen = new System.Windows.Forms.RichTextBox();
+            this.rsa_TaoKhoa2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rsa_TaoKhoa2);
+            this.groupBox1.Controls.Add(this.groupBox5);
+            this.groupBox1.Controls.Add(this.groupBox4);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Location = new System.Drawing.Point(30, 16);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1254, 236);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tạo khóa";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.soPhiN);
+            this.groupBox2.Controls.Add(this.soQ);
+            this.groupBox2.Controls.Add(this.soP);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(49, 40);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(407, 179);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.soE);
+            this.groupBox4.Controls.Add(this.soN);
+            this.groupBox4.Controls.Add(this.label5);
+            this.groupBox4.Controls.Add(this.label4);
+            this.groupBox4.Location = new System.Drawing.Point(524, 40);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(407, 179);
+            this.groupBox4.TabIndex = 1;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Khóa công khai";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.soD);
+            this.groupBox5.Controls.Add(this.label6);
+            this.groupBox5.Location = new System.Drawing.Point(990, 40);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(234, 179);
+            this.groupBox5.TabIndex = 1;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Khóa bí mật";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nguyên tố p";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nguyên tố q";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Phi n";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "n";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 17);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "e";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(18, 37);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(16, 17);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "d";
+            // 
+            // soP
+            // 
+            this.soP.Location = new System.Drawing.Point(141, 37);
+            this.soP.Name = "soP";
+            this.soP.Size = new System.Drawing.Size(146, 22);
+            this.soP.TabIndex = 1;
+            // 
+            // soQ
+            // 
+            this.soQ.Location = new System.Drawing.Point(141, 65);
+            this.soQ.Name = "soQ";
+            this.soQ.Size = new System.Drawing.Size(146, 22);
+            this.soQ.TabIndex = 1;
+            // 
+            // soPhiN
+            // 
+            this.soPhiN.Location = new System.Drawing.Point(141, 116);
+            this.soPhiN.Name = "soPhiN";
+            this.soPhiN.Size = new System.Drawing.Size(146, 22);
+            this.soPhiN.TabIndex = 1;
+            // 
+            // soN
+            // 
+            this.soN.Location = new System.Drawing.Point(143, 37);
+            this.soN.Name = "soN";
+            this.soN.Size = new System.Drawing.Size(146, 22);
+            this.soN.TabIndex = 1;
+            // 
+            // soE
+            // 
+            this.soE.Location = new System.Drawing.Point(143, 82);
+            this.soE.Name = "soE";
+            this.soE.Size = new System.Drawing.Size(146, 22);
+            this.soE.TabIndex = 1;
+            // 
+            // soD
+            // 
+            this.soD.Location = new System.Drawing.Point(21, 82);
+            this.soD.Name = "soD";
+            this.soD.Size = new System.Drawing.Size(146, 22);
+            this.soD.TabIndex = 1;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.rsa_banGiaiMa);
+            this.groupBox3.Controls.Add(this.btGiaiMa);
+            this.groupBox3.Controls.Add(this.rsa_banMaHoaGuiDen);
+            this.groupBox3.Controls.Add(this.rsa_BanMaHoa);
+            this.groupBox3.Controls.Add(this.btMaHoa);
+            this.groupBox3.Controls.Add(this.rsa_BanRo);
+            this.groupBox3.Location = new System.Drawing.Point(30, 267);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(1253, 261);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Mã hóa";
+            // 
+            // rsa_BanRo
+            // 
+            this.rsa_BanRo.Location = new System.Drawing.Point(27, 31);
+            this.rsa_BanRo.Name = "rsa_BanRo";
+            this.rsa_BanRo.Size = new System.Drawing.Size(527, 103);
+            this.rsa_BanRo.TabIndex = 0;
+            this.rsa_BanRo.Text = "";
+            // 
+            // btMaHoa
+            // 
+            this.btMaHoa.Location = new System.Drawing.Point(602, 79);
+            this.btMaHoa.Name = "btMaHoa";
+            this.btMaHoa.Size = new System.Drawing.Size(75, 23);
+            this.btMaHoa.TabIndex = 1;
+            this.btMaHoa.Text = "Mã hóa";
+            this.btMaHoa.UseVisualStyleBackColor = true;
+            this.btMaHoa.Click += new System.EventHandler(this.btMaHoa_Click);
+            // 
+            // rsa_BanMaHoa
+            // 
+            this.rsa_BanMaHoa.Location = new System.Drawing.Point(720, 31);
+            this.rsa_BanMaHoa.Name = "rsa_BanMaHoa";
+            this.rsa_BanMaHoa.Size = new System.Drawing.Size(527, 103);
+            this.rsa_BanMaHoa.TabIndex = 2;
+            this.rsa_BanMaHoa.Text = "";
+            // 
+            // rsa_banGiaiMa
+            // 
+            this.rsa_banGiaiMa.Location = new System.Drawing.Point(720, 145);
+            this.rsa_banGiaiMa.Name = "rsa_banGiaiMa";
+            this.rsa_banGiaiMa.Size = new System.Drawing.Size(527, 103);
+            this.rsa_banGiaiMa.TabIndex = 5;
+            this.rsa_banGiaiMa.Text = "";
+            // 
+            // btGiaiMa
+            // 
+            this.btGiaiMa.Location = new System.Drawing.Point(602, 193);
+            this.btGiaiMa.Name = "btGiaiMa";
+            this.btGiaiMa.Size = new System.Drawing.Size(75, 23);
+            this.btGiaiMa.TabIndex = 4;
+            this.btGiaiMa.Text = "Giải mã";
+            this.btGiaiMa.UseVisualStyleBackColor = true;
+            this.btGiaiMa.Click += new System.EventHandler(this.btGiaiMa_Click);
+            // 
+            // rsa_banMaHoaGuiDen
+            // 
+            this.rsa_banMaHoaGuiDen.Location = new System.Drawing.Point(27, 145);
+            this.rsa_banMaHoaGuiDen.Name = "rsa_banMaHoaGuiDen";
+            this.rsa_banMaHoaGuiDen.Size = new System.Drawing.Size(527, 103);
+            this.rsa_banMaHoaGuiDen.TabIndex = 3;
+            this.rsa_banMaHoaGuiDen.Text = "";
+            // 
+            // rsa_TaoKhoa2
+            // 
+            this.rsa_TaoKhoa2.Location = new System.Drawing.Point(289, 11);
+            this.rsa_TaoKhoa2.Name = "rsa_TaoKhoa2";
+            this.rsa_TaoKhoa2.Size = new System.Drawing.Size(125, 23);
+            this.rsa_TaoKhoa2.TabIndex = 2;
+            this.rsa_TaoKhoa2.Text = "Tạo khóa";
+            this.rsa_TaoKhoa2.UseVisualStyleBackColor = true;
+            this.rsa_TaoKhoa2.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1318, 555);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button rsa_TaoKhoa2;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.TextBox soD;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox soE;
+        private System.Windows.Forms.TextBox soN;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox soPhiN;
+        private System.Windows.Forms.TextBox soQ;
+        private System.Windows.Forms.TextBox soP;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RichTextBox rsa_banGiaiMa;
+        private System.Windows.Forms.Button btGiaiMa;
+        private System.Windows.Forms.RichTextBox rsa_banMaHoaGuiDen;
+        private System.Windows.Forms.RichTextBox rsa_BanMaHoa;
+        private System.Windows.Forms.Button btMaHoa;
+        private System.Windows.Forms.RichTextBox rsa_BanRo;
+    }
+}
+
---
+++ RSA/Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RSA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void reset_rsa()
+        {
+            soP.Text = soQ.Text = soPhiN.Text = soN.Text = soE.Text = soD.Text = string.Empty;
+
+        }
+        int RSA_soP, RSA_soQ, RSA_soN, RSA_soE, RSA_soD, RSA_soPhi_n;
+        public int RSA_d_dau = 0;
+        private int RSA_ChonSoNgauNhien()
+        {
+            Random rd = new Random();
+            return rd.Next(11, 101);// tốc độ chậm nên chọn số bé
+        }
+        //"Hàm kiểm tra nguyên tố"
+        private bool RSA_kiemTraNguyenTo(int xi)
+        {
+            bool kiemtra = true;
+            if (xi == 2 || xi == 3)
+            {
+                // kiemtra = true;
+                return kiemtra;
+            }
+            else
+            {
+                if (xi == 1 || xi % 2 == 0 || xi % 3 == 0)
+                {
+                    kiemtra = false;
+                }
+                else
+                {
+                    for (int i = 5; i <= Math.Sqrt(xi); i = i + 6)
+                        if (xi % i == 0 || xi % (i + 2) == 0)
+                        {
+                            kiemtra = false;
+                            break;
+                        }
+                }
+            }
+            return kiemtra;
+        }
+        // "Hàm kiểm tra hai số nguyên tố cùng nhau"
+        private bool RSA_nguyenToCungNhau(int ai, int bi)
+        {
+            bool ktx_;
+            // giải thuật Euclid;
+            int temp;
+            while (bi != 0)
+            {
+                temp = ai % bi;
+                ai = bi;
+                bi = temp;
+            }
+            if (ai == 1) { ktx_ = true; }
+            else ktx_ = false;
+            return ktx_;
+        }
+        // "Hàm lấy mod"
+        public int RSA_mod(int mx, int ex, int nx)
+        {
+
+            //Sử dụng thuật toán "bình phương nhân"
+            //Chuyển e sang hệ nhị phân
+            int[] a = new int[100];
+            int k = 0;
+            do
+            {
+                a[k] = ex % 2;
+                k++;
+                ex = ex / 2;
+            }
+            while (ex != 0);
+            //Quá trình lấy dư
+            int kq = 1;
+            for (int i = k - 1; i >= 0; i--)
+            {
+                kq = (kq * kq) % nx;
+                if (a[i] == 1)
+                    kq = (kq * mx) % nx;
+            }
+            return kq;
+        }
+
+        private void button3_Click(object sender, EventArgs e)//tao kkhoa
+        {
+            reset_rsa();
+            RSA_soP = RSA_soQ = 0;
+            do
+            {
+                RSA_soP = RSA_ChonSoNgauNhien();
+                RSA_soQ = RSA_ChonSoNgauNhien();
+            }
+            while (RSA_soP == RSA_soQ || !RSA_kiemTraNguyenTo(RSA_soP) || !RSA_kiemTraNguyenTo(RSA_soQ));
+            soP.Text = RSA_soP.ToString();
+            soQ.Text = RSA_soQ.ToString();
+            RSA_taoKhoa();
+            RSA_d_dau = 1;
+            //rsa_TaoKhoa.Content = "Tạo lại khóa mới";
+            rsa_TaoKhoa2.Enabled = false;
+            
+            btMaHoa.Enabled = true;
+            btGiaiMa.Enabled = true;
+        }
+
+        private void btMaHoa_Click(object sender, EventArgs e)
+        {
+            if (RSA_d_dau != 1)
+            { MessageBox.Show("Bạn chưa tạo khóa!", "Thông Báo"); }
+
+            else
+            {
+                if (rsa_BanRo.Text == "")
+                {
+                    MessageBox.Show("Bạn chưa nhập bản rõ để mã hóa!", "Thông Báo");
+                }
+
+                else
+                {
+                    // thực hiện mã hóa
+                    try
+                    {
+                        RSA_MaHoa(rsa_BanRo.Text);
+                        //btMaHoa.Enabled = false;
+                        //btGiaiMa.Enabled = true;
+                        //RSA_d_dau = 2;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Lỗi");
+                    }
+                }
+            }
+        }
+
+        private void btGiaiMa_Click(object sender, EventArgs e)
+        {
+
+            if (RSA_d_dau != 1)
+                MessageBox.Show("Bạn phải tạo khóa trước ", "Thông Báo");
+            else
+                try
+                {
+
+                    RSA_GiaiMa(rsa_banMaHoaGuiDen.Text);
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Lỗi");
+                }
+        }
+
+        private void toolStripComboBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private bool nguyenToCungNhau(int ai, int bi)// "Hàm kiểm tra hai số nguyên tố cùng nhau"
+        {
+            bool ktx_;
+            // giải thuật Euclid;
+            int temp;
+            while (bi != 0)
+            {
+                temp = ai % bi;
+                ai = bi;
+                bi = temp;
+            }
+            if (ai == 1) { ktx_ = true; }
+            else ktx_ = false;
+            return ktx_;
+        }
+        private void RSA_taoKhoa()
+        {
+            //Tinh n=p*q
+            RSA_soN = RSA_soP * RSA_soQ;
+            soN.Text = RSA_soN.ToString();
+            //Tính Phi(n)=(p-1)*(q-1)
+            RSA_soPhi_n = (RSA_soP - 1) * (RSA_soQ - 1);
+            soPhiN.Text = RSA_soPhi_n.ToString();
+            //Tính e là một số ngẫu nhiên có giá trị 0< e <phi(n) và là số nguyên tố cùng nhau với Phi(n)
+            do
+            {
+                if (soE.Text == "")
+                {
+                    Random RSA_rd = new Random();
+                    RSA_soE = RSA_rd.Next(2, RSA_soPhi_n);
+                }
+                else
+                {
+                    RSA_soE = int.Parse(soE.Text);
+                }
+
+            }
+            while (!nguyenToCungNhau(RSA_soE, RSA_soPhi_n));
+            soE.Text = RSA_soE.ToString();
+
+            //Tính d là nghịch đảo modular của e
+            RSA_soD = 0;
+            int i = 2;
+            while (((1 + i * RSA_soPhi_n) % RSA_soE) != 0 || RSA_soD <= 0)
+            {
+                i++;
+                RSA_soD = (1 + i * RSA_soPhi_n) / RSA_soE;
+            }
+            soD.Text = RSA_soD.ToString();
+        }
+        public void RSA_MaHoa(string ChuoiVao)
+        {
+            // taoKhoa();
+            // Chuyen xau thanh ma Unicode
+            byte[] mh_temp1 = Encoding.Unicode.GetBytes(ChuoiVao);
+            string base64 = Convert.ToBase64String(mh_temp1);
+
+            // Chuyen xau thanh ma Unicode
+            int[] mh_temp2 = new int[base64.Length];
+            for (int i = 0; i < base64.Length; i++)
+            {
+                mh_temp2[i] = (int)base64[i];
+            }
+
+            //Mảng a chứa các kí tự đã mã hóa
+            int[] mh_temp3 = new int[mh_temp2.Length];
+            for (int i = 0; i < mh_temp2.Length; i++)
+            {
+                mh_temp3[i] = RSA_mod(mh_temp2[i], RSA_soE, RSA_soN); // mã hóa
+            }
+
+            //Chuyển sang kiểu kí tự trong bảng mã Unicode
+            string str = "";
+            for (int i = 0; i < mh_temp3.Length; i++)
+            {
+                str = str + (char)mh_temp3[i];
+            }
+            byte[] data = Encoding.Unicode.GetBytes(str);
+            string a = Convert.ToBase64String(data);
+            rsa_BanMaHoa.Text = a;
+            rsa_banMaHoaGuiDen.Text = rsa_BanMaHoa.Text;
+
+        }
+        public void RSA_GiaiMa(string ChuoiVao)
+        {
+            byte[] temp2 = Convert.FromBase64String(ChuoiVao);
+            string giaima = Encoding.Unicode.GetString(temp2);
+
+            int[] b = new int[giaima.Length];
+            for (int i = 0; i < giaima.Length; i++)
+            {
+                b[i] = (int)giaima[i];
+            }
+            //Giải mã
+            int[] c = new int[b.Length];
+            for (int i = 0; i < c.Length; i++)
+            {
+                c[i] = RSA_mod(b[i], RSA_soD, RSA_soN);// giải mã
+            }
+
+            string str = "";
+            for (int i = 0; i < c.Length; i++)
+            {
+                str = str + (char)c[i];
+            }
+            byte[] data2 = Convert.FromBase64String(str);
+            rsa_banGiaiMa.Text = Encoding.Unicode.GetString(data2);
+
+        }
+
+
+
+
+    }
+}
